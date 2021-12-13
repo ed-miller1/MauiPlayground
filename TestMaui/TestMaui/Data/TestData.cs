1f4d946a9101a1380321f02be49055cd0738a4a1
Edward Miller <miller.edwardg@gmail.com>
2021-12-13 11:10:34 -0600

    adding maui project

---
+++ TestMaui/TestMaui/Data/TestData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaui.Data
+{
+    public class TestData
+    {
+        public Guid Id { get; set; }
+        public string Item1 { get;set; }
+        public string Item2 { get;set; }
+    }
+}
---
+++ TestMaui/TestMaui/Data/TestDataListService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaui.Data
+{
+    public class TestDataListService
+    {
+        public List<TestData> GetTestDataList()
+        {
+            return TestDataListSingleton.Instance;
+        }
+
+        public Task<List<TestData>> GetTestDataListAsync()
+        {
+            return Task.FromResult(TestDataListSingleton.Instance);
+        }
+
+        public Task AddDataItemAsync(TestData item)
+        {
+            AddDataItem(item);
+            return Task.CompletedTask;
+        }
+
+        public void AddDataItem(TestData item)
+        {
+            //var instance = TestDataListSingleton.Instance;
+            //instance.Add(item);
+            TestDataListSingleton.Instance.Add(item);
+        }
+    }
+
+    public sealed class TestDataListSingleton
+    {
+        private static readonly Lazy<List<TestData>> testDataSingleton = new Lazy<List<TestData>>(() => GenerateTestData());
+        public static List<TestData> Instance { get { return testDataSingleton.Value; } }
+        private TestDataListSingleton() { }
+        private static List<TestData> GenerateTestData()
+        {
+            var returnList = new List<TestData>();
+            for (int i = 0; i < 10; i++)
+            {
+                returnList.Add(new TestData()
+                {
+                    Id = Guid.NewGuid(),
+                    Item1 = $"Item1_{i}",
+                    Item2 = $"Item2_{i}"
+                });
+            }
+            return returnList;
+        }
+    }
+}
